326c1cc3ece33c35466f25548c49ee2cd1fedb35
Sir2fa <Chibuezegodwin49@gmail.com>
2023-02-22 14:26:02 -0800

    “print”

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - intial entry
+*
+* Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
+   return (0);
+}
