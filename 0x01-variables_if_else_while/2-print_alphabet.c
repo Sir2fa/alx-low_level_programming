e87e42bf899c53610244ef75b31359cd5dba34d6
Sir2fa <Chibuezegodwin49@gmail.com>
2023-02-24 10:07:03 -0800

    “number”

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*description: lowercase alphabet
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int n = 97;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar ('\n');
+return (0);
+}
