5873560b76352c6ecb292ff937cc02d77da151d4
Godwin_jnr <Chibuezegodwin49@gmail.com>
2025-10-26 18:46:30 +0100

    initial commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,23 +6,25 @@
 */
 int main(void)
 {
-int n, m;
-for (n = 47; n <= 57; n++)
-{
-for (m = 48; m <= 56; m++)
-{
-if (n > m)
-{
-putchar(m);
-putchar(n);
-if (n != 56 || m != 57)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int n;
+	int m;
+
+	n = 0;
+	while (n <= 8)
+	{
+		m = n + 1;
+		while (m <= 9)
+		{
+			putchar(n + '0');
+			putchar(m + '0');
+			if (n !=8 || m != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			m++;
+		}
+		n++;
+	}
+	return(0);
 }
