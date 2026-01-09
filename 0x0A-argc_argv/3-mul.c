562cba389945aa1831493af9259624f3ba5fbf27
Godwin_jnr <Chibuezegodwin49@gmail.com>
2026-01-09 16:04:07 +0100

    args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
-* main - Entry point
-* @argc: argument count
-* @argv: argument vector
-* Return: Always 0
-*/
+	#include <stdio.h>
+	#include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-int num1, num2, mul;
-if (argc != 3)
-{
-printf("Error\n");
-return (1);
-}
-else
-{
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-mul = num1 * num2;
-printf("%d\n", mul);
-}
-return (0);
-}
+	/**
+	* main - Entry point
+	* @argc: argument count
+	* @argv: argument vector
+	*
+	* Return:  0
+	*/
+
+	int main(int argc, char *argv[])
+	{
+		int num1, num2;
+	
+		if (argc < 3)
+		{	
+			printf("Error\n");
+			return (1);
+		}		
+	
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n",  num1 * num2);
+
+		return (0);
+	}
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
-* main - Entry point
-* @argv: argument vector
-* @argc: argument count
-* Return: Always 0
-*/
+	#include <stdio.h>
+	#include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-int i;
-int sum = 0;
+	/**
+	 * main - adds positive numbers.
+	 * @argc: argument count 
+	 * @argv : argument vector
+	 *
+	 * Return: 0
+	 */
+	int main(int argc, char **argv)
+	{	
+		int i, n, sum = 0;
+		char *flag;
+		
+		if (argc < 2)
+		{	
+			printf("0\n")
+			return(0);
+		}
+		
+		
+		for (i = 1; argv[i]; i++)
+		{	
+			n = strtol(argv[i], $flag, 10);
+			if (*flag)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum +=n;
+			}
+		}
+		printf("%d\n", sum);
 
-if (argc == 1)
-{
-printf("0\n");
-}
-else
-{
-for (i = 1; i < argc; i++)
-{
-if (!atoi(argv[i]))
-{
-printf("Error\n");
-return (1);
-}
-else
-sum = sum + atoi(argv[i]);
-}
-printf("%d\n", sum);
-}
-return (0);
-}
+		return (0);
+	}
