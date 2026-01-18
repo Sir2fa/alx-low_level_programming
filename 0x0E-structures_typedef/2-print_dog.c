13706012c02adb60d79daba1c0072481f85b6d8a
Godwin_jnr <Chibuezegodwin49@gmail.com>
2026-01-19 00:10:25 +0100

    structs_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,11 +15,11 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Age: %.6f\n", d->age);
+			printf("Name: (nil)\n");
 		printf("Age: %.6f\n", d->age);
 		if (d->owner != NULL)
 			printf("owner: %s\n", d->owner);
 		else
-			printf("owner: nil\n");
+			printf("owner: (nil)\n");
 	}
 }
