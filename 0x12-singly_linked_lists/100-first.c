302f88deb5407b86804f602e680289351ca70e75
Godwin_jnr <Chibuezegodwin49@gmail.com>
2026-02-07 02:31:47 +0100

    singly link list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * start - the string before the function execution.
+ * myStartupFun - Apply the constructor attribute to myStartupFun () so that it
+ * 	is excuted before main()
  */
-
 void start(void) __attribute__((constructor));
 
-
+/**
+ * start - implementing start
+ */
 void start(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
